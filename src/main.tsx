07b7f6b8283780b8f8d7796d9e1b4e75af09ad97
Hyrul <xavier.leonard@outlook.com>
2024-12-30 02:04:59 +0100

    added signup and login pages to router

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import './global.scss'
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
+import Signup from "./pages/Signup/Signup.tsx";
+import Login from "./pages/Login/Login.tsx";
 import Commendations from "./pages/Commendations/Commendations.tsx";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
 
@@ -14,6 +16,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Header />
     <Routes>
       <Route path='/Sot-Tracker' element={<Home />} />
+      <Route path='/Sot-Tracker/signup' element={<Signup />} />
+      <Route path='/Sot-Tracker/login' element={<Login />} />
       {/* <Route path='/SoT-Tracker/commendations' element={<Commendations />} /> */}
       <Route path='*' element={<ErrorPage />} /> 
     </Routes>
