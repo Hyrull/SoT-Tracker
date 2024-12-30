46e81d24ac9ddddb15e954cb54a2abd7c97fb8ad
Hyrul <xavier.leonard@outlook.com>
2024-12-30 02:23:20 +0100

    now displays log out if user is logged in

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,14 @@
 import { Link } from 'react-router'
-
 import './Header.scss'
 // import logo from './../../assets/img/sot-logo.png'
 import logo from './../../assets/img/sot-tracker-logo1.png'
+
+const token = localStorage.getItem('token')
+
+const removeToken = () => {
+  localStorage.removeItem('token')
+  window.location.href = '/SoT-Tracker'
+}
 
 function Header() {
   return (
@@ -11,7 +17,12 @@ function Header() {
       <nav data-itemtype="https://schema.org/SiteNavigationElement" data-itemscope="">
         <Link data-itemprop='url' to='/SoT-Tracker'>Home</Link>
         <Link data-itemprop='url' to='/SoT-Tracker/commendations'>Commendations</Link>
-        <Link data-itemprop='url' to='/SoT-Tracker/login'>Log in</Link>
+        {
+          token ? (
+            <Link data-itemprop='url' to='/SoT-Tracker/' onClick={removeToken}>Log out</Link>
+          ) : (
+            <Link data-itemprop='url' to='/SoT-Tracker/login'>Log in</Link>
+        )}
       </nav>
     </header>
   )
