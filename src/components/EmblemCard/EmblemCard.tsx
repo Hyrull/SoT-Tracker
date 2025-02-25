839b66c9a7e6cc45ddda12475193e4dbe8e277e7
Hyrul <xavier.leonard@outlook.com>
2025-02-25 19:46:50 +0100

    completed emblems now look different

--- src/components/EmblemCard/EmblemCard.tsx
+++ src/components/EmblemCard/EmblemCard.tsx
@@ -4,7 +4,7 @@ import './EmblemCard.scss'
 
 const EmblemCard: React.FC<EmblemCardProps> = ({ emblem, showRewards }) => {
   return (
-    <div className="emblem-card">
+    <div className={`emblem-card ${emblem.Completed ? 'completed' : ''}`} key={emblem.title}>
       <img loading="lazy" src={emblem.image} alt={emblem.title || 'Emblem'} />
       <div className="card-content">
         <h4>{emblem.title}</h4>
