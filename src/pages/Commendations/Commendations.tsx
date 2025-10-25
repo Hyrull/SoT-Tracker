a96fa432614e063b767b9531eddcb7f6705ca64c
Hyrul <xavier.leonard@outlook.com>
2025-10-25 02:18:17 +0200

    Refactor: Commendation now reads faction names from a dedicated file

--- src/pages/Commendations/Commendations.tsx
+++ src/pages/Commendations/Commendations.tsx
@@ -4,76 +4,12 @@ import { AllCommsData } from '../../types/types'
 import './Commendations.scss'
 import FiltersBar from './Components/FiltersBar'
 import FactionDropdown from './Components/FactionDropdown'
+import factionNames from './Data/FactionNames'
 
 
 // quick toggle for me when working the backend
 // const apiUrl = 'http://localhost:10000/api'
 const apiUrl = 'https://backend.sot-tracker.com/api'
-
-// Faction names object - I should put that in a dedicated file
-const factionNames: Record<string, {name: string, logo: string, banner: string}> = {
-  ReapersBones: {
-    name: "Reaper's Bones",
-    logo: "assets/img/faction logos/Reaper's_Bones_logo.webp",
-    banner: "assets/img/faction banners/Reaper's_Bones_banner.webp",
-  },
-  AthenasFortune: {
-    name: "Athena's Fortune",
-    logo: "assets/img/faction logos/Athena's_Fortune_logo.webp",
-    banner: "assets/img/faction banners/Athena's_Fortune_banner.webp",
-  },
-  OrderOfSouls: {
-    name: "Order of Souls",
-    logo: "assets/img/faction logos/Order_of_Souls_logo.webp",
-    banner: "assets/img/faction banners/Order_of_Souls_banner.webp",
-  },
-  BilgeRats: {
-    name: "Adventures at Sea",
-    logo: "assets/img/faction logos/Adventures_at_Sea_logo.webp",
-    banner: "assets/img/faction banners/Bilge_Rats_banner.webp",
-  },
-  CreatorCrew: {
-    name: "Creator Crew",
-    logo: "assets/img/faction logos/Creator_Crew_logo.webp",
-    banner: "assets/img/faction banners/Creator_Crew_banner.webp",
-  },
-  TallTales: {
-    name: "Tall Tales",
-    logo: "assets/img/faction logos/Tall_Tales_logo.webp",
-    banner: "assets/img/faction banners/Tall_Tales_banner.webp",
-  },
-  HuntersCall: {
-    name: "Hunter's Call",
-    logo: "assets/img/faction logos/The_Hunter's_Call_logo.webp",
-    banner: "assets/img/faction banners/The_Hunter's_Call_banner.webp",
-  },
-  MerchantAlliance: {
-    name: "Merchant Alliance",
-    logo: "assets/img/faction logos/Merchant_Alliance_logo.webp",
-    banner: "assets/img/faction banners/Merchant_Alliance_banner.webp",
-  },
-  SeaDogs: {
-    name: "Sea Dogs",
-    logo: "assets/img/faction logos/Sea_Dogs_logo.webp",
-    banner: "assets/img/faction banners/Sea_Dogs_banner.webp",
-  },
-  GoldHoarders: {
-    name: "Gold Hoarders",
-    logo: "assets/img/faction logos/Gold_Hoarders_logo.webp",
-    banner: "assets/img/faction banners/Gold_Hoarders_banner.webp",
-  },
-  PirateLord: {
-    name: "Guardians of Fortune",
-    logo: "assets/img/faction logos/Athena's_Fortune_logo.webp",
-    banner: "assets/img/faction banners/Guardians_of_Fortune_banner.webp",
-  },
-  Flameheart: {
-    name: "Servants of the Flame",
-    logo: "assets/img/faction logos/Reaper's_Bones_logo.webp",
-    banner: "assets/img/faction banners/Servants_of_the_Flame_banner.webp",
-  }
-}
-
 
 const Commendations = () => {
   const [emblems, setEmblems] = useState<AllCommsData>({})
