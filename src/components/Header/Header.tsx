241d0763e72a2a0cfc1f81ddfae0d1360d777eae
Hyrul <xavier.leonard@outlook.com>
2025-10-30 19:52:32 +0100

    Opti: Lightened Header logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router'
 import './Header.scss'
-import logo from '/assets/img/sot-tracker-logo1.png'
+// import logo from '/assets/img/sot-tracker-logo1.png'
+import logo from '/assets/img/sot-tracker-logo-light.webp'
 
 const token = localStorage.getItem('token')
 
