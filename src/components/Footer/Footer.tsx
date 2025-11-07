14b6d3a421f6ecfe2b5858a9c9407ece87b630e8
Hyrul <xavier.leonard@outlook.com>
2025-11-07 04:25:50 +0100

    Edit: Updated footer & source link is now noblank

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,14 @@
 import './Footer.scss'
+import externalLinkIcon from '/assets/img/icons/external_link.svg'
 
 function Footer() {
   return (
     <footer>
-    <p>Website made by Hyrul</p>
-    <a href='https://github.com/Hyrull/SoT-Tracker'>Source code</a>
+    <p>Website made by Hyrul & Freya</p>
+    <div className='code-link'>
+      <a href='https://github.com/Hyrull/SoT-Tracker' target="_blank" rel="noopener noreferrer">Source code</a>
+      <img src={externalLinkIcon} alt="External link" className="external-link-icon" />
+    </div>
     </footer>
   )
 }
