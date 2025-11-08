2c8bc15f50b988296456eeb461fb3688d31eece8
Hyrul <xavier.leonard@outlook.com>
2025-11-08 19:11:52 +0100

    Feat: Added a Toast component for global use

---
+++ src/components/Toast/Toast.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+import './Toast.scss'
+
+export interface ToastProps {
+  content: string,
+  duration?: number // has to be in ms, default is 3s / 3000ms
+}
+
+function Toast({ content, duration = 3000 }: ToastProps) {
+  const [visible, setVisible] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => setVisible(false), duration)
+    return () => clearTimeout(timer)
+  }, [duration])
+
+  if (!visible) return null
+
+  return (
+    <div className="toast-container"
+      style={{ ['--toast-duration' as string]: `${duration}ms` }}
+      role="status"
+      aria-live="polite"
+      >
+      <div className="toast-message">{content}</div>
+    </div>
+  )
+}
+
+export default Toast
