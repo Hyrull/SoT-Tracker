90695441cc5f1a4ee3cbcd63f1f09b0c07708a00
Hyrul <xavier.leonard@outlook.com>
2026-01-03 01:57:29 +0100

    Fix: Logo & banner paths for /commendations/ to find them regardless of it's access directly or from a relative path

--- src/pages/Commendations/Data/FactionNames.ts
+++ src/pages/Commendations/Data/FactionNames.ts
@@ -1,63 +1,63 @@
 const factionNames: Record<string, {name: string, logo: string, banner: string}> = {
   ReapersBones: {
     name: "Reaper's Bones",
-    logo: "assets/img/faction logos/Reaper's_Bones_logo.webp",
-    banner: "assets/img/faction banners/Reaper's_Bones_banner.webp",
+    logo: "/assets/img/faction logos/Reaper's_Bones_logo.webp",
+    banner: "/assets/img/faction banners/Reaper's_Bones_banner.webp",
   },
   AthenasFortune: {
     name: "Athena's Fortune",
-    logo: "assets/img/faction logos/Athena's_Fortune_logo.webp",
-    banner: "assets/img/faction banners/Athena's_Fortune_banner.webp",
+    logo: "/assets/img/faction logos/Athena's_Fortune_logo.webp",
+    banner: "/assets/img/faction banners/Athena's_Fortune_banner.webp",
   },
   OrderOfSouls: {
     name: "Order of Souls",
-    logo: "assets/img/faction logos/Order_of_Souls_logo.webp",
-    banner: "assets/img/faction banners/Order_of_Souls_banner.webp",
+    logo: "/assets/img/faction logos/Order_of_Souls_logo.webp",
+    banner: "/assets/img/faction banners/Order_of_Souls_banner.webp",
   },
   BilgeRats: {
     name: "Adventures at Sea",
-    logo: "assets/img/faction logos/Adventures_at_Sea_logo.webp",
-    banner: "assets/img/faction banners/Bilge_Rats_banner.webp",
+    logo: "/assets/img/faction logos/Adventures_at_Sea_logo.webp",
+    banner: "/assets/img/faction banners/Bilge_Rats_banner.webp",
   },
   CreatorCrew: {
     name: "Creator Crew",
-    logo: "assets/img/faction logos/Creator_Crew_logo.webp",
-    banner: "assets/img/faction banners/Creator_Crew_banner.webp",
+    logo: "/assets/img/faction logos/Creator_Crew_logo.webp",
+    banner: "/assets/img/faction banners/Creator_Crew_banner.webp",
   },
   TallTales: {
     name: "Tall Tales",
-    logo: "assets/img/faction logos/Tall_Tales_logo.webp",
-    banner: "assets/img/faction banners/Tall_Tales_banner.webp",
+    logo: "/assets/img/faction logos/Tall_Tales_logo.webp",
+    banner: "/assets/img/faction banners/Tall_Tales_banner.webp",
   },
   HuntersCall: {
     name: "Hunter's Call",
-    logo: "assets/img/faction logos/The_Hunter's_Call_logo.webp",
-    banner: "assets/img/faction banners/The_Hunter's_Call_banner.webp",
+    logo: "/assets/img/faction logos/The_Hunter's_Call_logo.webp",
+    banner: "/assets/img/faction banners/The_Hunter's_Call_banner.webp",
   },
   MerchantAlliance: {
     name: "Merchant Alliance",
-    logo: "assets/img/faction logos/Merchant_Alliance_logo.webp",
-    banner: "assets/img/faction banners/Merchant_Alliance_banner.webp",
+    logo: "/assets/img/faction logos/Merchant_Alliance_logo.webp",
+    banner: "/assets/img/faction banners/Merchant_Alliance_banner.webp",
   },
   SeaDogs: {
     name: "Sea Dogs",
-    logo: "assets/img/faction logos/Sea_Dogs_logo.webp",
-    banner: "assets/img/faction banners/Sea_Dogs_banner.webp",
+    logo: "/assets/img/faction logos/Sea_Dogs_logo.webp",
+    banner: "/assets/img/faction banners/Sea_Dogs_banner.webp",
   },
   GoldHoarders: {
     name: "Gold Hoarders",
-    logo: "assets/img/faction logos/Gold_Hoarders_logo.webp",
-    banner: "assets/img/faction banners/Gold_Hoarders_banner.webp",
+    logo: "/assets/img/faction logos/Gold_Hoarders_logo.webp",
+    banner: "/assets/img/faction banners/Gold_Hoarders_banner.webp",
   },
   PirateLord: {
     name: "Guardians of Fortune",
-    logo: "assets/img/faction logos/Guardians_of_Fortune_logo.webp",
-    banner: "assets/img/faction banners/Guardians_of_Fortune_banner.webp",
+    logo: "/assets/img/faction logos/Guardians_of_Fortune_logo.webp",
+    banner: "/assets/img/faction banners/Guardians_of_Fortune_banner.webp",
   },
   Flameheart: {
     name: "Servants of the Flame",
-    logo: "assets/img/faction logos/Servants_of_The_Flame_logo.webp",
-    banner: "assets/img/faction banners/Servants_of_the_Flame_banner.webp",
+    logo: "/assets/img/faction logos/Servants_of_The_Flame_logo.webp",
+    banner: "/assets/img/faction banners/Servants_of_the_Flame_banner.webp",
   }
 }
 
