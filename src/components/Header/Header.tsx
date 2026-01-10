867ff00f8041575d8e425d1305fd4330b66fe498
Hyrul <xavier.leonard@outlook.com>
2026-01-10 21:47:53 +0100

    Feat: Added a banner for when the backend's offline

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { Link, NavLink, useNavigate } from 'react-router'
+import { useState, useEffect } from 'react'
 import './Header.scss'
 import logo from '/assets/img/sot-tracker-logo-large-light.webp'
 import logoNoText from '/assets/img/sot-tracker-logo-small-light.webp'
@@ -6,19 +7,57 @@ import scoreIcon from "/assets/img/icons/icon-tier-4.svg"
 import { useUser } from '../../contexts/UserContext'
 
 
-
 function Header() {
   const { score, username, token, logout } = useUser()
   const navigate = useNavigate()
+  
+  const apiUrl = 'https://backend.sot-tracker.com/api'
+  const [backendOnline, setBackendOnline] = useState(true)
 
   const handleLogout = (e: React.MouseEvent) => {
-    e.preventDefault() // Stop the link navigation for a moment
+    e.preventDefault()
     logout()
     navigate('/')
   }
 
+  useEffect(() => {
+    // just checking if the server's up
+    const checkHealth = async () => {
+      const controller = new AbortController()
+      const timeoutId = setTimeout(() => controller.abort(), 3000)
+
+      try {
+        const response = await fetch(`${apiUrl}/health`, { 
+          method: 'HEAD', // head gets no body. faster
+          signal: controller.signal 
+        })
+        
+        clearTimeout(timeoutId);
+        
+        setBackendOnline(response.ok)
+        
+      } catch (error) {
+        // if the request didn't go through, then server is down
+        setBackendOnline(false)
+      }
+    }
+
+    checkHealth()
+    const interval = setInterval(checkHealth, 10 * 1000) // 10s
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
   return (
     <header>
+      {backendOnline ? 
+      <div className='no-backend-banner'>
+        <p className='no-backend-text'>SoT Tracker servers are offline. Please try again later.</p>
+        <p className='no-backend-subtext'>Blame my ISP. If it's been offline for a while, ping @hyrul on Discord!</p>
+      </div>
+      : null }
       <div className='header-content'>
         <Link to='/'>
           <img 
