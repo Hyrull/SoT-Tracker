30bf58d0001a664c211e90dc8c96d78c973dd3d3
Hyrul <xavier.leonard@outlook.com>
2026-03-09 01:27:41 +0100

    Edit: Updated example data

--- src/pages/Commendations/Components/DemoBanner.tsx
+++ src/pages/Commendations/Components/DemoBanner.tsx
@@ -11,7 +11,7 @@ const DemoBanner: React.FC<DemoBannerProps> = ({ isDemo }) => {
 
   return (
     <div className="demo-banner">
-      <p>You are currently browsing a demo version of this page, using Hyrul's data as of the 21st of November, 2025.<br/>
+      <p>You are currently browsing a demo version of this page, using Hyrul's data.<br/>Last update: March 8th, 2026 (Season 18).<br/>
       <Link to="/signup">Sign Up</Link> or <Link to="/login">Log In</Link> to browse your own commendations.</p>
     </div>
   )
