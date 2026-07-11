a7f4eec13a921df435ffb96ad50c9fc51b5fbb35
Hyrul <xavier.leonard@outlook.com>
2026-07-11 02:15:29 +0200

    Fix: Updated demo data (they changed the comm img endpoint)

--- src/pages/Commendations/Components/DemoBanner.tsx
+++ src/pages/Commendations/Components/DemoBanner.tsx
@@ -11,7 +11,7 @@ const DemoBanner: React.FC<DemoBannerProps> = ({ isDemo }) => {
 
   return (
     <div className="demo-banner">
-      <p>You are currently browsing a demo version of this page, using Hyrul's data.<br/>Last update: March 8th, 2026 (Season 18).<br/>
+      <p>You are currently browsing a demo version of this page, using Hyrul's data.<br/>Last update: July 10th, 2026 (Season 20).<br/>
       <Link to="/signup">Sign Up</Link> or <Link to="/login">Log In</Link> to browse your own commendations.</p>
     </div>
   )
